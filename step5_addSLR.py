dfcfae49f6b3072d035764fbca96a1e1bfd6e135
Nick Young <nick.young@auckland.ac.nz>
2025-12-18 12:34:53 +1300

    Switch from parquet to CSV, use SLR CSV for confidence intervals

--- step5_addSLR.py
+++ step5_addSLR.py
@@ -1,47 +1,32 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov  4 19:11:02 2025
-
-@author: yaxio
-"""
 import os
 import pandas as pd
 import geopandas as gpd
 
 # input loc
-shorelinepointfilename = "JaMoNoRaSoWa"  # JaMoNoRaSoWa
-inputfolderloc = r"output/brunnrule"
 buffer_dist = 10
 confidence_level = "medium_confidence"
 year = 2150
 percentile = "0.5"
 scenario = 4.5
-inputfilename = f"brunnrule_{shorelinepointfilename}_buffer_{buffer_dist}.gpkg"
-outputfolderloc = r"output/brunnrule"
 
-SLR_loc = r"input/SLR"
-SLR_matchfile = f"{shorelinepointfilename}_SLR_match.gpkg"
-
-SLR_scenariofile = f"{confidence_level}_y_{year}_s_{scenario}.gpkg"
+# Note the typo here, brunnrule instead of bruunrule
+inputfilename = f"../output/brunnrule/brunnrule_JaMoNoRaSoWa_buffer_{buffer_dist}.gpkg"
+SLR_matchfile = f"../input/SLR/JaMoNoRaSoWa_SLR_match.gpkg"
+SLR_scenariofile = f"../input/SLR/{confidence_level}_y_{year}_s_{scenario}.gpkg"
 currentyear = 2020  # The current year is used to calculate the erosion rate. Ideally, it should correspond to the mean date of the most recent shoreline points.
 
-# current folder
-current_script = os.path.abspath(__file__)
-grandparent_folder = os.path.dirname(os.path.dirname(current_script))
-
 # SLR data
-SLR_match = gpd.read_file(os.path.join(grandparent_folder, SLR_loc, SLR_matchfile))
-SLR_scenario = gpd.read_file(
-    os.path.join(grandparent_folder, SLR_loc, SLR_scenariofile)
-)
-print(os.path.join(grandparent_folder, SLR_loc, SLR_matchfile), SLR_match.crs)
-print(os.path.join(grandparent_folder, SLR_loc, SLR_scenariofile), SLR_scenario.crs)
-data = gpd.read_file(os.path.join(grandparent_folder, inputfolderloc, inputfilename))
-print(os.path.join(grandparent_folder, inputfolderloc, inputfilename), data.crs)
+SLR_match = gpd.read_file(SLR_matchfile)
+SLR_scenario = gpd.read_file(SLR_scenariofile)
+print(SLR_matchfile, SLR_match.crs)
+print(SLR_scenariofile, SLR_scenario.crs)
+data = gpd.read_file(inputfilename)
+print(inputfilename, data.crs)
 SLR_scenario = SLR_scenario.to_crs(data.crs)
-print(os.path.join(grandparent_folder, SLR_loc, SLR_scenariofile), SLR_scenario.crs)
+print(SLR_scenariofile, SLR_scenario.crs)
 years_delta = year - currentyear
+print(years_delta)
 
 # Vectorized lookup to avoid per-row filtering
 # Ensure indices are unique before mapping
@@ -53,9 +38,9 @@ scenario_lookup = SLR_scenario.drop_duplicates(subset="Site ID").set_index("Site
 ]
 
 data["Site ID"] = data["Unique_ID"].map(site_lookup)
-data["S_SLR"] = data["Site ID"].map(scenario_lookup)
-data["R_SLR"] = data["S_SLR"] * data["R"]
-data["ER_SLR"] = data["R_SLR"] / years_delta
+#data["S_SLR"] = data["Site ID"].map(scenario_lookup)
+#data["R_SLR"] = data["S_SLR"] * data["R"]
+#data["ER_SLR"] = data["R_SLR"] / years_delta
 
 data["lat"] = data.geometry.to_crs(4326).y
 data["lon"] = data.geometry.to_crs(4326).x
@@ -73,26 +58,14 @@ cols_for_map = [
     "L",
     "R",
     "tanbeta",
-    "S_SLR",
-    "R_SLR",
-    "ER_SLR",
+    "Site ID",
+    #"S_SLR",
+    #"R_SLR",
+    #"ER_SLR",
     "lat",
     "lon",
     "wave_lat",
     "wave_lon",
 ]
 print(data[cols_for_map])
-data[cols_for_map].to_parquet(
-    os.path.join(
-        f"brunnrule_{shorelinepointfilename}_b_{buffer_dist}_{confidence_level}_y_{year}_s_{scenario}_p_{percentile}.parquet"
-    )
-)
-
-# data.to_file(
-#     os.path.join(grandparent_folder, outputfolderloc, outputfilename_shp),
-#     driver="ESRI Shapefile",
-# )
-# data.to_file(
-#     os.path.join(grandparent_folder, outputfolderloc, outputfilename_gpkg),
-#     driver="GPKG",
-# )
+data[cols_for_map].to_csv(f"bruunrule_JaMoNoRaSoWa_b_{buffer_dist}.csv", index=False, float_format="%.6f")
